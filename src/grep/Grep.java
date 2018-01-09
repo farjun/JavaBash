e2b3d219e5260ab8ca731878590e084a84d3f0e1
omer <omerbh1@gmail.com>
2018-01-09 02:35:07 +0200

    first setup

---
+++ src/grep/Grep.java
@@ -0,0 +1,13 @@
+package grep;
+
+import java.io.*;
+import java.util.ArrayList;
+
+public class Grep{
+
+    public static ArrayList<String> go(String[] args,Boolean[] flags,ArrayList<BufferedReader> fileReaders){
+
+        return null;
+    }
+
+}
